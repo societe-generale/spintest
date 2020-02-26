c9b8c79f3de95ce323b8b27dff1ed9fc88643aaa
svandenb080618 <simon.vandenbulcke@socgen.com>
2020-02-26 15:53:01 +0000

    feat: Create reports generation and associated test

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -4,6 +4,7 @@ import httpretty
 import pytest
 import shutil
 from spintest import logger, spintest
+from urllib.parse import urlparse
 
 logger.disabled = True
 
@@ -16,6 +17,30 @@ def create_delete_report_dir():
     os.makedirs(REPORT_DIR, exist_ok=True)
     yield
     shutil.rmtree(REPORT_DIR)
+
+
+def read_report(report_path):
+    with open(report_path, "r") as f:
+        spintest_report = json.load(f)
+
+        result = []
+        for iteration in range(len(spintest_report)):
+            url = urlparse(spintest_report[iteration]["url"])
+            if url.scheme in ["http", "https"]:
+                result.append("True")
+            else:
+                result.append("False")
+            for number in range(len(spintest_report[iteration]["reports"])):
+                output = spintest_report[iteration]["reports"][number]["output"]
+                if "**" in output["__token__"]:
+                    result.append("True")
+                else:
+                    result.append("False")
+
+        if "False" in result:
+            return False
+        else:
+            return True
 
 
 def test_manager_basic_generate_report():
@@ -39,6 +64,38 @@ def test_manager_basic_generate_report():
     assert True is result
 
     assert True is os.path.isfile(report_path)
+
+    assert True is read_report(report_path)
+
+    httpretty.disable()
+    httpretty.reset()
+
+
+def test_manager_basic_generate_report_with_token():
+    """Test spintest with generate report"""
+    report_path = os.path.join(REPORT_DIR, "basic_report_token.json")
+    token = "ABC"
+    httpretty.enable()
+    httpretty.register_uri(
+        httpretty.GET, "http://test.com/test", body=json.dumps({"foo": "bar"})
+    )
+    httpretty.register_uri(httpretty.GET, "http://test.com/bar")
+
+    result = spintest(
+        ["http://test.com"],
+        [
+            {"method": "GET", "route": "/test", "output": "test"},
+            {"method": "GET", "route": "/{{ test['foo'] }}"},
+        ],
+        generate_report=report_path,
+        token=token,
+    )
+
+    assert True is result
+
+    assert True is os.path.isfile(report_path)
+
+    assert True is read_report(report_path)
 
     httpretty.disable()
     httpretty.reset()
