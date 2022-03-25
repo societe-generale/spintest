dda51fa749ffc254898284106de73c551ceba4e3
svandenb080618 <simon.vandenbulcke@socgen.com>
2022-03-25 09:46:14 +0000

    chore: create test with rollback value

--- tests/test_rollback.py
+++ tests/test_rollback.py
@@ -40,22 +40,27 @@ def test_rollback_unkwnown_ref_parallel():
 
 def test_rollback_with_output_value():
     """Test spintest with unknown rollback reference."""
+    httpretty.enable()
+    httpretty.register_uri(httpretty.POST, "http://test.com/test", status=201,body={"id": "1234-5678"})
+    httpretty.register_uri(httpretty.DELETE, "http://test.com/test", status=201)
+
     result = spintest(
         ["http://test.com"],
         [
             {
                 "method": "POST",
                 "route": "/test",
-                # "output": "test_output",
-                "expected": {"code": 400},
+                "output": "test_output",
+                "expected": {"code": 201},
                 "rollback": ["test_rollback"],
             },
-            # {"name" : "test_rollback", "method": "DELETE", "route": "/test/{{ test_output['id'] }}"},
-            {"name" : "test_rollback", "method": "DELETE", "route": "/test"},
+            {"name" : "test_rollback", "method": "DELETE", "route": "/test/{{ test_output['id'] }}"},
         ],
     )
+    httpretty.disable()
+    httpretty.reset()
 
-    assert result is False
+    assert result is True
 
 
 def test_rollback_invalid_schema():
